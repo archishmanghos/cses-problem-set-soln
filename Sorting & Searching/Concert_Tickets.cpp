027c56f001aa9e701d987356be01126981df1fe6
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-09-20 12:45:02 +0530

    Delete collecting numbers

--- Sorting & Searching/Concert_Tickets.cpp
+++ Sorting & Searching/Concert_Tickets.cpp
@@ -1,24 +1,51 @@
-//Practice
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
-
 #include <bits/stdc++.h>
-#define ll long long
+#define int long long
 using namespace std;
-const ll N = 2e5 + 5;
-const ll INF = 1e9 + 5;
-const ll mxN = 1000000007;
-const double E = 1e-9;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
 
-//------------------------------------------------------------------------------
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
 int n, a, m;
+
 int32_t main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	/*ifstream cin("input.txt");
-	ofstream cout("output.txt");*/
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	    freopen("error.txt", "w", stderr);
+	#endif
+
 	cin >> n >> m;
 	vector<int> b(m);
 	multiset<int, greater<int>> sa;
@@ -40,7 +67,6 @@ int32_t main()
 			sa.erase(it);
 		}
 	}
-	return 0;
+
+    return 0;
 }
-//22-08-2020 11:07:45
-//------------------------------------------------------------------------------
